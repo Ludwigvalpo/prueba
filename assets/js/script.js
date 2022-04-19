69fa05e15603df234d653179f0f86ca31f231960
Ludwig Valdenegro <ludwigval@gmail.com>
2022-04-19 02:06:26 -0400

    ultimos retoques, ventana modal y tooltips

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+   
+
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
